5508ebd4dad2ce385f3da186f47139e1347514d0
PITT1 <petroleo.plta.97@gmail.com>
2024-07-07 18:35:55 -0400

    cambios esteticos

--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,25 +1,27 @@
-import PropTypes from 'prop-types'
+import PropTypes from "prop-types";
 
-const Item = ({ motor, hp, calibre, cliente, llegada, reparacion }) => {
+const Item = ({ id, motor, hp, calibre, cliente, llegada, reparacion }) => {
   return (
     <tr>
-      <td>{motor}</td>
-      <td>{hp}</td>
-      <td>{calibre}</td>
-      <td>{cliente}</td>
-      <td>{llegada}</td>
-      <td>{reparacion}</td>
+      <td className="text-center">{id}</td>
+      <td className="text-center">{motor}</td>
+      <td className="text-center">{hp}</td>
+      <td className="text-center">{calibre}</td>
+      <td className="text-center">{cliente}</td>
+      <td className="text-center">{llegada}</td>
+      <td className="text-center">{reparacion}</td>
     </tr>
   );
 };
 
 Item.propTypes = {
-    motor: PropTypes.string.isRequired,
-    hp: PropTypes.number.isRequired,
-    calibre: PropTypes.number.isRequired,
-    cliente: PropTypes.string.isRequired,
-    llegada: PropTypes.string.isRequired,
-    reparacion: PropTypes.string.isRequired
-}
+  id: PropTypes.number.isRequired,
+  motor: PropTypes.string.isRequired,
+  hp: PropTypes.number.isRequired,
+  calibre: PropTypes.number.isRequired,
+  cliente: PropTypes.string.isRequired,
+  llegada: PropTypes.string.isRequired,
+  reparacion: PropTypes.string.isRequired,
+};
 
 export default Item;
