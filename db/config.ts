78c1a0e8681a24312fc8dd548b2f14ceeb91e65a
PITT1 <petroleo.plta.97@gmail.com>
2024-07-09 11:31:52 -0400

    definiendo las tablas SQL

--- db/config.ts
+++ db/config.ts
@@ -1,6 +1,25 @@
-import { defineDb } from 'astro:db';
+import { column, defineDb, defineTable } from 'astro:db';
 
 // https://astro.build/db/config
+
+const Motor = defineTable({
+  columns:{
+    marca: column.text(),
+    hp: column.number(),
+    rpm: column.number(),
+    volt: column.number(),
+    amp: column.number(),
+    modelo: column.text()
+  }
+});
+
+const Clientes = defineTable({
+  columns:{
+    fecha: column.date(),
+    nombre: column.text()
+  }
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Motor, Clientes }
 });
