9945da12b512d41a0c1d98c11a1edfc2ab5d42f9
PITT1 <petroleo.plta.97@gmail.com>
2024-07-21 17:57:42 -0400

    componente historial creado

---
+++ src/components/HistorialItem.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+const HistorialItem = ({ cliente, fecha, motor, observacion }) => {
+  return (
+       <tr>
+					<td class="py-3 text-center">{cliente}</td>
+					<td class="py-3 text-center">{fecha}</td>
+					<td class="py-3 text-center">{motor}</td>
+					<td class="py-3 text-center">{observacion}</td>
+				</tr>
+  );
+};
+
+HistorialItem.propTypes = {
+  nombre: PropTypes.string.isRequired,
+  cliente: PropTypes.string.isRequired,
+  fecha: PropTypes.string.isRequired,
+  motor: PropTypes.string.isRequired,
+  observacion: PropTypes.isRequired
+};
+
+export default HistorialItem;
